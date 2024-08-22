8921db3dea56ebe7a7f16e5630e59c56c0f86841
SonJunPyo <jp1598@ajou.ac.kr>
2024-08-22 10:12:18 +0900

    캐릭터 추가

--- Assets/02_Scripts/Scene04_Scripts/GameManagerLB.cs
+++ Assets/02_Scripts/Scene04_Scripts/GameManagerLB.cs
@@ -4,15 +4,24 @@ using UnityEngine.XR.Interaction.Toolkit;
 public class GameManagerLB : MonoBehaviour
 {
     public GameObject[] objectsToActivate = new GameObject[5];
+    public AudioClip selectSound;  // 재생할 사운드 클립
 
     // 이 메서드는 XRGrabInteractable의 selectEntered 이벤트에 연결됩니다.
     public void OnSelectEntered(SelectEnterEventArgs args)
     {
         GameObject selectedObject = args.interactableObject.transform.gameObject;
 
+        // 오브젝트를 활성화합니다.
         for (int i = 0; i < 5; i++)
         {
             objectsToActivate[i].SetActive(true);
-        }   
+        }
+
+        // selectSound가 할당된 경우 사운드를 재생합니다.
+        if (selectSound != null)
+        {
+            // 오브젝트의 위치에서 사운드를 재생합니다.
+            AudioSource.PlayClipAtPoint(selectSound, selectedObject.transform.position);
+        }
     } 
-}  
+}
---
+++ Assets/02_Scripts/VRRig.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+[System.Serializable]
+public class VRMap
+{
+    public Transform vrTarget;
+    public Transform ikTarget;
+    public Vector3 trackingPositionOffset;
+    public Vector3 trackingRotationOffset;
+    public void Map()
+    {
+        ikTarget.position = vrTarget.TransformPoint(trackingPositionOffset);
+        ikTarget.rotation = vrTarget.rotation * Quaternion.Euler(trackingRotationOffset);
+    }
+}
+
+public class VRRig : MonoBehaviour
+{
+    [Range(0,1)]
+    public float turnSmoothness = 0.1f;
+    public VRMap head;
+    public VRMap leftHand;
+    public VRMap rightHand;
+
+    public Vector3 headBodyPositionOffset;
+    public float headBodyYawOffset;
+
+    void LateUpdate()
+    {
+        transform.position = head.ikTarget.position + headBodyPositionOffset;
+        float yaw = head.vrTarget.eulerAngles.y;
+        transform.rotation = Quaternion.Lerp(transform.rotation,Quaternion.Euler(transform.eulerAngles.x, yaw, transform.eulerAngles.z),turnSmoothness);
+
+        head.Map();
+        leftHand.Map();
+        rightHand.Map();
+    }
+}
